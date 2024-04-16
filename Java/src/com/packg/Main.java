36f5f9467aacf263d5efee6fddd63e8b20d7f7bf
Horraira <abuhorraira1432@gmail.com>
2024-04-16 08:37:28 +0600

    java setup

---
+++ Java/src/com/packg/Main.java
@@ -0,0 +1,21 @@
+package com.packg;
+// package in java is the collection of classes and interfaces that are bundled together as they are related to each other
+public class Main {
+    // public class and method means they are accessible from outside the class and package and can be called from anywhere in the project
+    // static means that the method belongs to the class and not any object of the class and can be called without creating an object of the class
+    // void means that the method does not return any value and is used just to perform an action
+    // main is the name of the method and is the entry point of the program and is called by the JVM when the program is executed
+    // String[] args is the parameter of the main method and is used to accept command line arguments
+    // throws Exception is used to handle exceptions that may occur during the execution of the program
+    public static void main(String[] args) throws Exception {
+        // creating an object of the User class and passing the name of the user as an argument to the constructor
+         User user = new User("John");
+         System.out.println(user.name);
+
+        // create a new object of the User class without constructor
+        User user2 = new User("John");
+        user2.age = "52";
+        System.out.println(user2.age);
+
+    }
+}
---
+++ Java/src/com/packg/User.java
@@ -0,0 +1,14 @@
+package com.packg;
+
+public class User {
+    // public means that the variable is accessible from outside the class and package
+    // attribute of the class is a variable that is associated with an object and is used to define the properties of an object
+    public String name;
+    public String age;
+
+    // Constructor is a special type of method that is used to initialize the object
+    // It is called when an object of the class is created and memory is allocated for the object
+     public User(String name) {
+         this.name = name;
+     }
+}
